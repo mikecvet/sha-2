64f6032f96aa6894c62e1547f2455bf8a53706b6
Mike Cvet <mike.lp.cvet@gmail.com>
2023-07-15 20:20:16 -0700

    Working sha2

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,10 @@ impl Default for State {
 }
 
 impl State {
+    /**
+     * Rotates state values according to https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf 6.2.2 s
+     * section 3.
+     */
     fn rotate (&mut self, x: u32, y: u32) {
        self.h = self.g;
        self.g = self.f;
@@ -97,6 +101,16 @@ impl State {
     }
 }
 
+/**
+ * See https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf # 5.1.2
+ * 
+ * Suppose the length of the message M, in bits, is  bits. Append the bit “1” to the end of the message, 
+ * followed by k zero bits, where k is the smallest non-negative solution to the equation L + 1 + k = 896 mod 1024. 
+ * Then append the 128-bit block that is equal to the number L expressed using a binary representation. 
+ * For example, the (8-bit ASCII) message “abc” has length 8 x 3 = 24, so the message is padded with a one bit,
+ * then 896 - (24 + 1) = 871 zero bits, and then the message length, to become the 1024-bit padded message.
+ * The length of the padded message should now be a multiple of 1024 bits.
+ */
 fn
 pad (message: &mut Vec<u8>) {
     let mlen_in_bits = message.len() * 8 % MAX_LEN;
@@ -119,49 +133,80 @@ hash (message: &str) -> String {
     let mut state:State = Default::default();
     let mut message_bytes = message.as_bytes().to_vec();
 
+    // Extend to a multiple of 512 bits
     pad (&mut message_bytes);
 
-    let b64_string = base64::encode(&message_bytes);
-    println!("input message: {}", b64_string);
-
-    let s = message_bytes.iter().map(|byte| byte.to_string()).collect::<Vec<String>>().join(" ");
-    println!("input message: {}", s);
-
-    // 512-bit chunks
+    /*
+    * From https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf # 5.2
+    * 
+    * For SHA-1, SHA-224 and SHA-256, the message and its padding are parsed into N 512-bit blocks, M(1), M(2),..., M(N). 
+    * Since the 512 bits of the input block may be expressed as sixteen 32-bit words, the first 32 bits of message 
+    * block i are denoted M0(i), the next 32 bits are M1(i), and so on up to M(i).
+    * 
+    * For SHA-384, SHA-512, SHA-512/224 and SHA-512/256, the message and its padding are parsed into N 1024-bit blocks, 
+    * M(1), M(2),..., M(N). Since the 1024 bits of the input block may be expressed as sixteen 64-bit words, the first 
+    * 64 bits of message block i are denoted M0(i), the next 64 bits are M(i), and so on up to M(i).
+    */
     for outer_block in message_bytes.chunks(64) {
         let mut w: [u32; 64] = [0; 64];
         let mut indx = 0;
 
-        // Fill M array with 32-bit integer from the outer 512-bit chunk
+        // Fill first 16 elements of w array with 32-bit integer from the 512-bit block
+        // See https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf # 6.2.2
         for chunk in outer_block.chunks(4) {
-            // Convert bytes into little-endian u32 integer and insert into m[indx]
+            // Convert message byte chunks into a big-endian u32 integer and insert into w[indx]
             let (b1, b2, b3, b4) = (chunk[0] as u32, chunk[1] as u32, chunk[2] as u32, chunk[3] as u32);
-            // w[indx] = (b4 << 24) | (b3 << 16) | (b2 << 8) | b1;
-            w[indx] = b4 | (b3 << 8) | (b2 << 16) | (b1 << 24);
+            w[indx] = (b1 << 24) | (b2 << 16) | (b3 << 8) | b4;
             indx += 1;
         }
 
         // 16 .. 63
         while indx < 64 {
-            let s0 = (w[indx - 15].rotate_right(7)) ^ (w[indx - 15].rotate_right(18)) ^ (w[indx - 15] >> 3);
-            let s1 = (w[indx - 2].rotate_right(17)) ^ (w[indx - 2].rotate_right(19)) ^ (w[indx - 2] >> 10);
+            /* 
+            * From https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf # 4.1.2
+            *
+            * The two functions σ0 and σ1 as defined in the specification.
+            */
+            let s0 = (w[indx - 15].rotate_right(7)) 
+                        ^ (w[indx - 15].rotate_right(18)) 
+                        ^ (w[indx - 15] >> 3);
+            let s1 = (w[indx - 2].rotate_right(17)) 
+                        ^ (w[indx - 2].rotate_right(19)) 
+                        ^ (w[indx - 2] >> 10);
 
-            w[indx] = w[indx - 16].wrapping_add(s0).wrapping_add(w[indx - 7]).wrapping_add(s1);
+            // From https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf # 6.2.2            
+            w[indx] = w[indx - 16]
+                        .wrapping_add(s0)
+                        .wrapping_add(w[indx - 7])
+                        .wrapping_add(s1);
             indx += 1;
         }
 
+        // Stored to add back to the state after the main processing loop
         let input_values: [u32; 8] = [state.a, state.b, state.c, state.d, state.e, state.f, state.g, state.h];
-
         indx = 0;
-        while indx < 64 {
-            let s1 = state.e.rotate_right(6) ^ state.e.rotate_right(11) ^ state.e.rotate_right(25);
-            let ch = (state.e & state.f) ^ ((!state.e) & state.g);
-            let temp1 = state.h.wrapping_add(s1).wrapping_add(ch).wrapping_add(K[indx]).wrapping_add(w[indx]);
-            let s0 = state.a.rotate_right(2) ^ state.a.rotate_right(13) ^ state.a.rotate_right(22);
-            let maj = (state.a & state.b) ^ (state.a & state.c) ^ (state.b & state.c);
-            let temp2 = s0.wrapping_add(maj);
 
-            state.rotate(temp1, temp2);
+        // See https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf # 6.2.2
+        while indx < 64 {
+            /* 
+            * From https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf # 4.1.2
+            *
+            * The functions Σ0, Σ1, Ch(x, y, z) and Maj(x, y, z)
+            */
+            let s0 = state.a.rotate_right(2) ^ state.a.rotate_right(13) ^ state.a.rotate_right(22);
+            let s1 = state.e.rotate_right(6) ^ state.e.rotate_right(11) ^ state.e.rotate_right(25);
+
+            let ch = (state.e & state.f) ^ ((!state.e) & state.g);
+            let maj = (state.a & state.b) ^ (state.a & state.c) ^ (state.b & state.c);
+
+            // See https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf # 6.2.2 section 3
+            state.rotate(
+                state.h.wrapping_add(s1)
+                  .wrapping_add(ch)
+                  .wrapping_add(K[indx])
+                  .wrapping_add(w[indx]),
+                s0.wrapping_add(maj)
+            );
 
             indx += 1;
         }
@@ -171,9 +216,6 @@ hash (message: &str) -> String {
 
     // Export state integers into a byte array
     let digest: [u8; 32] = state.export().try_into().expect("Wrong length");
-
-    let s = digest.iter().map(|byte| byte.to_string()).collect::<Vec<String>>().join(" ");
-    println!("output digest: {}", s);
 
     // Encode into base 64
     return hex::encode(&digest); 
